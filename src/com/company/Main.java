4a67c0abf134f761d42d7ce5286e056a697b3329
Kamil Kaczowka <s21790-pj@pjwstk.edu.pl>
2020-05-04 12:40:46 +0200

    add project

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+
+        System.out.println(a+b);
+    }
+}
---
+++ src/com/company/Pies.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Pies {
+    private String name;
+
+    public Pies(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
